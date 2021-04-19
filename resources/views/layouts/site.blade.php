26d42ee997160eb56fe98ecb35a0496fe2e91fc6
Christopher Emms <christopherjohnemms@gmail.com>
2021-04-19 09:09:01 +0100

    google tag manager

--- resources/views/layouts/site.blade.php
+++ resources/views/layouts/site.blade.php
@@ -23,9 +23,17 @@
             font-family: 'Open Sans', sans-serif;
         }
     </style>
+
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-5M7KMSG');</script>
+    <!-- End Google Tag Manager -->
 </head>
 
 <body class="antialiased">
+
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-5M7KMSG" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
 
     <button class="hamburger hamburger--collapse" type="button">
       <span class="hamburger-box">
